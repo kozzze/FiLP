29d6fc1644e8c3a33fa6b868903bb230df2fa8b3
kozzze <kozlov.ed.vs@mail.ru>
2025-03-26 22:53:45 +0300

    lab 5.2 quadratic solve

---
+++ FiLP/lab5.fs
@@ -0,0 +1,24 @@
+﻿open System
+
+let hello =
+    //Console.WriteLine("Hello")
+    0
+
+let solveQ a b c =
+    let d = b*b - 4.*a*c
+    if d > 0. then
+        let x1 = (-b + sqrt(d)) / (2.*a)
+        let x2 = (-b - sqrt(d)) / (2.*a)
+        Some(x1,x2)
+    elif d = 0. then
+        let x = -b / (2.*a)
+        Some(x,x)
+    else
+        None
+let res = solveQ 1. 2. -3.
+let resPrint()=
+    match res with
+    | Some(x1, x2) when x1 = x2 -> printfn $"Уравнение имеет один корень: {x1}"
+    | Some(x1, x2) -> printfn $"Решение: {x1}, {x2}"
+    | None -> printfn "Нет вещественных корней"
+resPrint()
