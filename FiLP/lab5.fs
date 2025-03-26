739672cdd66ddf8174b158531fdfb6c26bc8663d
kozzze <kozlov.ed.vs@mail.ru>
2025-03-26 23:07:51 +0300

    lab 5.2 S circle & V cylinder

--- FiLP/lab5.fs
+++ FiLP/lab5.fs
@@ -4,6 +4,8 @@ let hello =
     //Console.WriteLine("Hello")
     0
 
+//2
+
 let solveQ a b c =
     let d = b*b - 4.*a*c
     if d > 0. then
@@ -21,4 +23,21 @@ let resPrint()=
     | Some(x1, x2) when x1 = x2 -> printfn $"Уравнение имеет один корень: {x1}"
     | Some(x1, x2) -> printfn $"Решение: {x1}, {x2}"
     | None -> printfn "Нет вещественных корней"
-resPrint()
+
+//resPrint()
+
+open System
+
+let circleS (r: float) : float =
+    Math.PI * r * r
+
+let cylinderV (r: float) (h: float) : float =
+    circleS r * h
+
+let cirCylPrint () =
+    let s = circleS 3.0
+    let v = cylinderV 3.0 2.0
+    printfn $"Площадь круга = {s}"
+    printfn $"Объём цилиндра = {v}"
+
+cirCylPrint ()
