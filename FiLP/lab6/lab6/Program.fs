f534ccd3b5e2d0bd40d82801211d6e762537e516
kozzze <kozlov.ed.vs@mail.ru>
2025-04-09 18:16:47 +0300

    add max in [a,b]

--- FiLP/lab6/lab6/Program.fs
+++ FiLP/lab6/lab6/Program.fs
@@ -15,7 +15,7 @@
 //     | (head : int)::tail -> 
 //                        System.Console.WriteLine(head)
 //                        writeList tail  
-
+open System
 let max2 x y = if x > y then x else y
 
 let rec accCond list (f : int -> int -> int) p acc = 
@@ -185,6 +185,31 @@ let isLocalMinList index list =
     else
         false
         
+//25
+
+let findMaxInRange a b list =
+    list
+    |> List.filter (fun x -> x >= a && x <= b)  
+    |> List.max 
+let findMaxInRangeChurch a b list =
+    let rec filterInRange lst =
+        match lst with
+        | [] -> []
+        | h::t -> 
+            if h >= a && h <= b then h :: filterInRange t
+            else filterInRange t
+
+    let rec findMax lst maxVal =
+        match lst with
+        | [] -> maxVal
+        | h::t -> 
+            if h > maxVal then findMax t h
+            else findMax t maxVal
+
+    let filteredList = filterInRange list
+    if List.isEmpty filteredList then None  
+    else Some (findMax filteredList Int32.MinValue)
+      
         
 [<EntryPoint>]
 let main argv = 
@@ -203,10 +228,17 @@ let main argv =
     
     let arr = [3; 1; 4; 1; 5; 9; 2; 6; 5; 3]
     let index = 3
+    let a = 2
+    let b = 6
     // let resultList = isLocalMinList index arr
     // printfn "Метод List: Элемент с индексом %d %s локальный минимум" index (if resultList then "является" else "не является")
     // let resultChurch = isLocalMin index arr
     // printfn "Метод Church: Элемент с индексом %d %s локальный минимум" index (if resultChurch then "является" else "не является")
-    
+    // let resultList = findMaxInRange a b arr
+    // printfn "Максимальный элемент в интервале [%d, %d] с использованием List: %d" a b resultList
+    // let resultChurch = findMaxInRangeChurch a b arr
+    // match resultChurch with
+    // | Some maxVal -> printfn "Максимальный элемент в интервале [%d, %d] с использованием Church List: %d" a b maxVal
+    // | None -> printfn "Нет элементов в интервале [%d, %d]" a b
     
     0
