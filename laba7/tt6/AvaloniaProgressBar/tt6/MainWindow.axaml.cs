77145cc2468d72295321679475100d3756e5e4f8
kozzze <kozlov.ed.vs@mail.ru>
2025-04-16 22:24:42 +0300

    lab7 t6 !winform zato avalonia

---
+++ laba7/tt6/AvaloniaProgressBar/tt6/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace tt6;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
---
+++ laba7/tt6/tt6/Program.fs
@@ -0,0 +1,56 @@
+﻿open Avalonia
+open Avalonia.Controls
+open Avalonia.Controls.ApplicationLifetimes
+open Avalonia.Layout
+open Avalonia.Themes.Fluent
+
+type MainWindow() as this =
+    inherit Window()
+    do
+        this.Title <- "ProgressBar"
+        this.Width <- 600.0
+        this.Height <- 100.0
+
+        // Прогресс-бар (прижат к низу)
+        let progressBar = ProgressBar()
+        progressBar.Maximum <- 50.0
+        progressBar.Value <- 0.0
+        progressBar.Height <- 20.0
+        DockPanel.SetDock(progressBar, Dock.Bottom)
+
+        // Текстовое поле
+        let textBox = TextBox()
+        textBox.Width <- 300.0
+        textBox.Height <- 30.0
+        textBox.Margin <- Thickness(150.0, 20.0, 0.0, 0.0)
+        textBox.MaxLength <- 25
+
+        // Обработчик изменений текста
+        let changes _ =
+            let value = float (textBox.Text.Length * 2)
+            progressBar.Value <- min value progressBar.Maximum
+
+        textBox.TextChanged.Add(changes)
+
+        // Контейнер для элементов
+        let panel = DockPanel()
+        panel.Children.Add(textBox)
+        panel.Children.Add(progressBar)
+        this.Content <- panel
+
+type App() =
+    inherit Application()
+    override this.Initialize() =
+        this.Styles.Add(FluentTheme())
+    override this.OnFrameworkInitializationCompleted() =
+        match this.ApplicationLifetime with
+        | :? IClassicDesktopStyleApplicationLifetime as desktop ->
+            desktop.MainWindow <- MainWindow()
+        | _ -> ()
+
+[<EntryPoint>]
+let main argv =
+    AppBuilder
+        .Configure<App>()
+        .UsePlatformDetect()
+        .StartWithClassicDesktopLifetime(argv)
