f03d4466053f1aa69135085fa16eca7bd1a70497
kozzze <kozlov.ed.vs@mail.ru>
2025-04-23 22:42:28 +0300

    prolog l1 t2

--- FiLP/prolog/lr1/lab1.pl
+++ FiLP/prolog/lr1/lab1.pl
@@ -10,3 +10,10 @@ brothers(X) :- brother(X,Y), Y\=X, print(Y),nl,fail.
 b_s(X,Y) :- parent(Z,X), parent(Z,Y), Y\=X, nl.
 b_s(X) :-  setof(Y, b_s(Y, X), Siblings), write(Siblings).
 
+% 2
+
+father(X,Y):- parent(X,Y),man(X).
+father(X):- parent(Y,X),man(Y),print(Y).
+
+sister(X,Y):- parent(Z,X),parent(Z,Y),woman(X).
+sister(X):- parent(P,X),parent(P,Y),woman(Y),print(Y),nl,fail.
